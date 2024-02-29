6c3a3f81a73d2e8749cdb810dba733c21402cbc9
CuttleFish <b9804320975@gmail.com>
2024-02-29 15:28:32 +0800

    init ignore

---
+++ src/main/java/dodo/taiwanweather/TaiwanHistoricalWeatherData10YearApplication.java
@@ -0,0 +1,13 @@
+package dodo.taiwanweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaiwanHistoricalWeatherData10YearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaiwanHistoricalWeatherData10YearApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dodo/taiwanweather/TaiwanHistoricalWeatherData10YearApplicationTests.java
@@ -0,0 +1,13 @@
+package dodo.taiwanweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaiwanHistoricalWeatherData10YearApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
