211ce500f300a12ace309badb2ad0bfd25205ade
CuttleFish <b9804320975@gmail.com>
2024-03-02 18:12:14 +0800

    圖表 雨量、風速、氣壓

---
+++ src/main/resources/static/js/indexJS/airPressure.js
@@ -0,0 +1,45 @@
+function processAirPressureData(data) {
+    const labels = data.map(item => `${item.Month}月`);
+    const airPressureValues = data.map(item => item.AirPressure);
+    return {
+        labels: labels,
+        airPressureValues: airPressureValues
+    };
+}
+
+function renderAirPressureChart(processedData) {
+    let myChart = echarts.init(document.getElementById('airPressureResultSection'));
+
+    let option = {
+        title: {
+            text: '氣壓 (hPa)',
+            left: 'center'
+        },
+        tooltip: {
+            trigger: 'axis'
+        },
+        xAxis: {
+            type: 'category',
+            data: processedData.labels
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [{
+            data: processedData.airPressureValues,
+            type: 'line',
+            smooth: true,
+            areaStyle: {
+                color: 'green'
+            },
+            itemStyle: {
+                color: 'green'
+            },
+            lineStyle: {
+                color: 'green'
+            }
+        }]
+    };
+
+    myChart.setOption(option);
+}
